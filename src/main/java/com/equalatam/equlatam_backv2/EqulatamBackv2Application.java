b1751fa42ba575c73b06b4cf9225c9627655c282
samirt <sfuenmayor@ecuasanitas.com>
2026-01-20 14:14:03 -0500

    Initial Spring Boot API

---
+++ src/main/java/com/equalatam/equlatam_backv2/EqulatamBackv2Application.java
@@ -0,0 +1,13 @@
+package com.equalatam.equlatam_backv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqulatamBackv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EqulatamBackv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/equalatam/equlatam_backv2/EqulatamBackv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equalatam.equlatam_backv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqulatamBackv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
